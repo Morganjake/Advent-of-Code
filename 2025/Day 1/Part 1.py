c5dc6a1c99912737d2d921cceff66094fb1b23f0
Jake <morgan.jake888@gmail.com>
2025-12-03 11:59:15 +0000

    Completed Day 1 Part 2

---
+++ 2025/Day 1/Part 1.py
@@ -0,0 +1,16 @@
+import time
+
+startTime = time.process_time()
+
+with open("../2025/Day 1/input.txt", "r") as file:
+    code = file.readlines()
+
+zeroes = 0
+dial = 50
+
+for i in code:
+    dial = (dial + (int(i[1:]) if i[0] == "R" else int(i[1:]) * -1)) % 100
+    if dial == 0: zeroes += 1
+
+print(zeroes)
+print(f"PRogram finished in {time.process_time()- startTime:.4f}ms")
