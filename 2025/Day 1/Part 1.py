04ddcb208ac6c4b3db536062696befec4be8714a
Jake <morgan.jake888@gmail.com>
2025-12-04 11:45:14 +0000

    Completed Day 2 Part 1

--- 2025/Day 1/Part 1.py
+++ 2025/Day 1/Part 1.py
@@ -13,4 +13,4 @@ for i in code:
     if dial == 0: zeroes += 1
 
 print(zeroes)
-print(f"PRogram finished in {time.process_time()- startTime:.4f}ms")
+print(f"Program finished in {time.process_time() - startTime:.4f}ms")
---
+++ 2025/Day 2/Part 1.py
@@ -0,0 +1,21 @@
+import time
+
+startTime = time.process_time()
+
+with open("../2025/Day 2/input.txt", "r") as file:
+    Ids = [x.split("-") for x in file.readline().split(",")]
+
+invalidSum = 0
+
+for id1, id2 in Ids:
+    invalidIDHalf = int(id1[:len(id1) // 2]) if len(id1) % 2 == 0 else 10 ** (len(id1) // 2)
+    invalidID = int(str(invalidIDHalf) * 2)
+
+    while invalidID <= int(id2):
+        if invalidID >= int(id1):
+            invalidSum += invalidID
+        invalidIDHalf += 1
+        invalidID = int(str(invalidIDHalf) * 2)
+
+print(invalidSum)
+print(f"Program finished in {time.process_time() - startTime:.4f}ms")
