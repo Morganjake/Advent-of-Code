3fa765799d40d4ed8b919f936dfab624ce7e1ee8
Jake <morgan.jake888@gmail.com>
2025-12-05 14:26:07 +0000

    Completed Day 3 Part 1

---
+++ 2025/Day 3/Part 1.py
@@ -0,0 +1,18 @@
+import time
+
+startTime = time.perf_counter()
+
+with open("../2025/Day 3/input.txt", "r") as file:
+    voltages = file.readlines()
+
+volts = 0
+
+for i in voltages:
+    maxVolts = [0, 0]
+    for j, v in enumerate(i[:-2]):
+        if int(v) > maxVolts[0]: maxVolts = [int(v), j]
+
+    volts += int(str(maxVolts[0]) + str(max(i[maxVolts[1] + 1:])))
+
+print(volts)
+print(f"Program finished in {time.perf_counter() - startTime:.4f}ms")
