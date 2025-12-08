4679057fbcba729ce196d80212371a16e71b4cf8
Jake <morgan.jake888@gmail.com>
2025-12-08 15:53:25 +0000

    Completed Day 6 Part 1

---
+++ 2025/Day 6/Part 1.py
@@ -0,0 +1,24 @@
+import time
+
+startTime = time.perf_counter()
+
+with open("../2025/Day 6/input.txt", "r") as file:
+    unorderedNums = [[i for i in x.strip().split(" ") if i] for x in file.readlines()]
+
+nums = []
+for i in range(len(unorderedNums[0])):
+    nums.append([])
+    for j in range(len(unorderedNums)):
+        nums[i].append(unorderedNums[j][i])
+
+totalSum = 0
+
+for x in nums:
+    if x[-1] == "+": totalSum += sum([int(x) for x in x[:-1]])
+    else:
+        product = 1
+        for j in x[:-1]: product *= int(j)
+        totalSum += product
+
+print(totalSum)
+print(f"Program finished in {time.perf_counter() - startTime:.4f}ms")
